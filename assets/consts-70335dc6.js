e772506892e9989810b9973244b61a7141917d51
KW-M <16263357+KW-M@users.noreply.github.com>
2023-05-23 03:37:01 +0000

    Deploying to gh-pages from @ KW-M/rov-web@338456e003916cfcd042580242ba1a07a16a0cd7 🚀

---
+++ assets/consts-70335dc6.js
@@ -0,0 +1,2 @@
+const o=new TextEncoder,s=e=>o.encode(e),t=new TextDecoder,c=e=>t.decode(e),n="https://rov-web.livekit.cloud",E="http://localhost:7880",T="proxy:",O={autoSubscribe:!1,maxRetries:1e3,peerConnectionTimeout:45e3};export{c as D,s as E,n as L,T as P,E as a,O as b};
+//# sourceMappingURL=consts-70335dc6.js.map
---
+++ assets/serviceworker-9960a665.js
@@ -0,0 +1,2 @@
+addEventListener("install",e=>{console.log("SW: intalled")});addEventListener("navigation",e=>{console.log("Nav event: ",e)});addEventListener("fetch",e=>{console.log("fetch event: ",e)});
+//# sourceMappingURL=serviceworker-9960a665.js.map
