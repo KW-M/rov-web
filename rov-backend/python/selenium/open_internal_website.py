848f4e64e2d75cf47c4e7149f6b4dc0221326d9d
Ryan Anderson <rkanderson15@gmail.com>
2023-05-31 14:03:25 -0700

    updated selenium script, can now fetch title of webpage, still needs modularization

--- rov-backend/python/selenium/open_internal_website.py
+++ rov-backend/python/selenium/open_internal_website.py
@@ -1,22 +1,35 @@
+
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-import chromedriver_py
+from pyvirtualdisplay import Display
 
+display = Display(visible=0, size=(800, 600))
+display.start()
 
 chrome_options = Options()
 # chrome_options.add_experimental_option("detach", True)
 browser_binary_path = '/usr/bin/chromium-browser'
 
-driver_path = chromedriver_py.binary_path
+driver_path = '/usr/lib/chromium-browser/chromedriver'
 
 chrome_options.binary_location=browser_binary_path
+# chrome_options.add_argument("--headless")
+chrome_options.add_argument("--use-fake-ui-for-media-stream")
 
 driver = webdriver.Chrome(driver_path, options=chrome_options)
-# driver = webdriver.Chrome()
 
-driver.get("http://localhost:5173/backend/index.html?ForceLocal=false&RovRoomName=ROV123&CloudAPIKey=APIkoE7m3Zqd5dJ&CloudSecretKey=YbHcJZmAAbuI4S5Ba0LHAaXx6v9kfAlyLnviB2aRWSG&LocalAPIKey=NOTSET&LocalSecretKey=NOTSET")
+driver.get("https://kw-m.github.io/internal_irov_website/backend/index.html")
+
+# This is useful for testing
 # driver.get("https://www.selenium.dev/selenium/web/web-form.html")
+
+
+
+# Here I run a test to just grab the title of the webpage and quit
+title = driver.title
+print("The title of this page is ", title)
 
 try:
 	while True:
@@ -25,4 +38,3 @@ finally:
 	print("executing finally block...")
 	driver.quit()
 	print("quit driver")
-
