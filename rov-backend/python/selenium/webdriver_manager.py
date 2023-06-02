ac999a3b88483d9c6d20f9b80839c4d67b166997
Ryan Anderson <rkanderson15@gmail.com>
2023-06-01 20:17:11 -0700

    added untested code for a method in webdriver _manager.py that would check the webdriver periodically for issues and restart if any issue has been found. currently commented out so we can test all the current code first.

--- rov-backend/python/selenium/webdriver_manager.py
+++ rov-backend/python/selenium/webdriver_manager.py
@@ -1,11 +1,12 @@
 
+import asyncio
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from pyvirtualdisplay import Display
 # import socket
 # import httplib
-from selenium.webdriver.remote.command import Command
+# from selenium.webdriver.remote.command import Command
 
 
 
@@ -15,10 +16,11 @@ from selenium.webdriver.remote.command import Command
 class WebdriverManager:
 	
     def init(self):
-        pass
+        self.running = False
 
     
     def start(self):     
+        self.running = True
 
         # necessary to make virtual display to run graphical applications in headless mode
         self.display = Display(visible=0, size=(800, 600))
@@ -41,8 +43,23 @@ class WebdriverManager:
         self.driver = webdriver.Chrome(driver_path, options=chrome_options)
         self.driver.get("https://kw-m.github.io/internal_irov_website/backend/index.html") #TODO should point to appropriate website. (M) if it's static content couldn't we fetch it from the local file system?
 
+    """This function will periodically check if the driver is alive while the manager is set to running
+       if this isn't the case (which could maybe happen because the browser crashes or there's a network issue),
+         then it'll try to fix everything by just restarting.
+    """
+    # async def manage_driver_loop(self):
+    #     while True:
+    #         await asyncio.sleep(10) # arbitrary timeout for periodic checks
+    #         alive = self.driver_is_alive()
+    #         running = self.running
+    #         if running and not alive:
+    #             print("We have a problem, for some reason the webdriver is supposed to be running but it's not. Maybe the browser has crashed? or there's a network issue? Attempting to restart webdriver.")
+    #             self.stop()
+    #             self.start()
+            
 
     def stop(self):
+        self.running = False
         self.driver.quit()
 
     def test_fetch_title(self):
@@ -51,7 +68,7 @@ class WebdriverManager:
 
 
     """returns true if the webdriver is running, false otherwise"""
-    def is_running(self):
+    def driver_is_alive(self):
         # return "unknown"
         try:
             self.driver.title
