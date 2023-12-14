b8b1770887a7432c02728ed53acc80d7544be046
KW-M <16263357+KW-M@users.noreply.github.com>
2023-12-14 16:05:13 +0000

    Deploying to gh-pages from @ KW-M/rov-web@2650ad1d7b09655d1379437af9586d3c981977aa 🚀

--- assets/serviceworker-9960a665.js
+++
@@ -1,2 +0,0 @@
-addEventListener("install",e=>{console.log("SW: intalled")});addEventListener("navigation",e=>{console.log("Nav event: ",e)});addEventListener("fetch",e=>{console.log("fetch event: ",e)});
-//# sourceMappingURL=serviceworker-9960a665.js.map
